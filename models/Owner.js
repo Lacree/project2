6941fd43e99e9bdc0894ce09cd692214bff8bb54
Lacree Cross <lacree21willams@yahoo.com>
2019-10-22 09:34:15 -0400

    Working on Products Model now movieg to Owner

---
+++ models/Owner.js
@@ -0,0 +1,38 @@
+const mongoose = require('./connection.js')
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    city: String,
+    state: String
+})
+
+const UserCollection = mongoose.model('user', UserSchema)
+
+const getAllUsers = () => {
+    return UserCollection.find({})
+}
+
+const getOneUser = (id) => {
+    return UserCollection.findById(id)
+}
+
+const createUser = (userData) => {
+    return UserCollection.create(userData)
+}
+
+const updateUser = (id, userData) => {
+    return UserCollection.updateUser({ _id: id })
+}
+
+const deleteUser = (id) => {
+    return UserCollection.deleteOne({ _id: id })
+}
+
+module.exports = {
+    getAllUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser
+}
---
+++ models/Product.js
@@ -0,0 +1,38 @@
+const mongoose = require('./product.js')
+
+const ProductSchema = new mongoose.Schema({
+    name: String,
+    ratio: Number,
+    company: String,
+    Notes: String
+})
+
+const ProductCollection = mongoose.model('product', ProductSchema)
+
+const getAllProducts = () => {
+    return ProductCollection.find({})
+}
+
+const getOneProduct = (id) => {
+    return ProductCollection.findById(id)
+}
+
+const createProduct = (productData) => {
+    return ProductCollection.create(productData)
+}
+
+const updateProduct = (id, productData) => {
+    return ProductCollection.updateProduct({ _id: id })
+}
+
+const deleteProduct = (id) => {
+    return ProductCollection.deleteOne({ _id: id })
+}
+
+module.exports = {
+    getAllProducts,
+    getOneProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
