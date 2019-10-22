4eb5f2a029ddaf78e4e4ed0595d8a42c6cd1e5d0
Lacree Cross <lacree21willams@yahoo.com>
2019-10-22 10:18:49 -0400

    Almost Complete with controllers

--- controllers/owner.js
+++ controllers/owner.js
@@ -15,3 +15,37 @@ ownerRouter.get('/owner/new', (req, res) => {
     res.render('createOwnerForm')
 })
 
+//getAll
+ownerRouter.get('/owner', (req, res) => {
+    OwnerApi.getAllOwners()
+        .then((allOwners) => {
+            res.render('allOwners', { allOwners })
+        })
+})
+
+//getOne
+ownerRouter.get('/owner/:id', (req, res) => {
+    OwnerApi.getOneOwner(req.params.id)
+        .then((singleOwner) => {
+
+            res.render('singleOwner', singleOwner)
+        })
+})
+
+//update
+
+ownerRouter.put('/owner/:id', (req, res) => {
+    OwnerApi.updateOwner(req.params.id, req.body)
+        .then((updateOwner) => {
+            res.redirect(`/owner/${req.params.id}`)
+        })
+})
+
+
+//create
+ownerRouter.post('/owner', (req, res) => {
+    OwnerApi.createOwner(req.body)
+        .then((createdOwner) => {
+            res.redirect('/owner')
+        })
+})
